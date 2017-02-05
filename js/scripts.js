73e0522b2cc6a84b2d4191654bccdf202b25cb42
Oleg Torokhov <torokhov093@gmail.com>
2017-02-05 21:48:33 +0700

    add minus step

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 function range(a, b, step) {
   var arr = [];
   
-  if (step) {
+  if (step && step > 0) {
     for(var i = a; i <= b; i+=step) {
       arr.push(i);
-    } 
+    }
+  } else if (step && step < 0) {
+    for(var i = a; i >= b; i+=step) {
+      arr.push(i);
+    }
   } else {
     for(var i = a; i <= b; i++) {
       arr.push(i);
