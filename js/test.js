f86f079a3692585d9de706731c02634d4edd7400
Oleg Torokhov <torokhov093@gmail.com>
2017-02-05 21:31:43 +0700

    add new test func 'range'. А predetermined pitch range

--- js/test.js
+++ js/test.js
@@ -2,4 +2,8 @@ describe("range", function() {
   it("генерирует диапазон", function() {
     assert.deepEqual(range(1, 5), [1,2,3,4,5]);
   });
+  
+  it("генерирует диапазон с заданным шагом", function() {
+    assert.deepEqual(range(1, 10, 2), [1,3,5,7,9]);
+  });
 });
