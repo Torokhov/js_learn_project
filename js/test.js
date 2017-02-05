17c10f8cbef9ad6c6b494d4d155948ff52ad85ee
Oleg Torokhov <torokhov093@gmail.com>
2017-02-05 21:43:41 +0700

    add test for func 'range', minus step

--- js/test.js
+++ js/test.js
@@ -6,4 +6,8 @@ describe("range", function() {
   it("генерирует диапазон с заданным шагом", function() {
     assert.deepEqual(range(1, 10, 2), [1,3,5,7,9]);
   });
+  
+  it("генерирует диапазон с отрицательным шагом", function() {
+    assert.deepEqual(range(5, 2, -1), [5, 4, 3, 2]);
+  });
 });
